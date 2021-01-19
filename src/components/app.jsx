76a4c2faa996bd79b294458b1a43b68e7732c545
Alena Gritsiuk <alena22292@mail.ru>
2021-01-19 12:14:21 +0100

    Create an App class with state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current: '',
+      previous: []
+    };
+  }
+
   render() {
     return (
-      <div>
-        Hello!
+      <div className="app">
+        <h1>Calculator</h1>
+        <input className="result" type="text" value={this.state.current} />
       </div>
     );
   }
