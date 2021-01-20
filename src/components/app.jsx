3cd58614fb2d40f400fb2399bd4940047bd70fb3
Alena Gritsiuk <alena22292@mail.ru>
2021-01-20 10:01:16 +0100

    Add math functions to the calc

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Buttons from './buttons.jsx';
+import Buttons from './buttons';
 
 class App extends Component {
   constructor(props) {
@@ -7,16 +7,36 @@ class App extends Component {
 
     this.state = {
       current: '0',
-      previous: []
+      previous: [],
+      next: false
     };
   }
 
   reset = () => {
-    this.setState({ result: '0' });
+    this.setState({ current: '0' });
   }
 
   addToCurrent = (val) => {
-    this.setState({ current: this.state.current + val });
+    console.log(val);
+    if (["/", "-", "+", "*"].indexOf(val) > -1) {
+      const { previous } = this.state;
+      previous.push(this.state.current + val);
+      this.setState({ previous, next: true });
+    } else {
+      if ((this.state.current === "0" && val !== ".") || this.state.next) {
+        this.setState({ current: val, next: false });
+      } else {
+        this.setState({ current: this.state.current + val });
+      }
+    }
+  }
+
+  calculate = (val) => {
+    let { previous, current, next} = this.state;
+    if (previous.length > 0) {
+      current = eval(String(previous[previous.length - 1] + current));
+      this.setState({ current, previous, next: true });
+    }
   }
 
   render() {
@@ -26,7 +46,7 @@ class App extends Component {
       { symbol: '7', cols: 1, action: this.addToCurrent },
       { symbol: '8', cols: 1, action: this.addToCurrent },
       { symbol: '9', cols: 1, action: this.addToCurrent },
-      { symbol: 'x', cols: 1, action: this.addToCurrent },
+      { symbol: '*', cols: 1, action: this.addToCurrent },
       { symbol: '4', cols: 1, action: this.addToCurrent },
       { symbol: '5', cols: 1, action: this.addToCurrent },
       { symbol: '6', cols: 1, action: this.addToCurrent },
@@ -37,10 +57,11 @@ class App extends Component {
       { symbol: '+', cols: 1, action: this.addToCurrent },
       { symbol: '0', cols: 2, action: this.addToCurrent },
       { symbol: '.', cols: 1, action: this.addToCurrent },
-      { symbol: '=', cols: 1, action: this.addToCurrent }
+      { symbol: '=', cols: 1, action: this.calculate }
     ];
     return (
       <div className="app">
+        { this.state.previous.length > 0 ? <div className="floaty-last">{this.state.previous[this.state.previous.length - 1]}</div> : null }
         <h1>Calculator</h1>
         <input className="result" type="text" value={this.state.current} />
         {buttons.map((btn, i) => {
